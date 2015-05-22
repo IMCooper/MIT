95173944e5ec438e99e88f7a22238ca29323e06d
Ross Kynch <w0ss4g3@gmail.com>
2015-05-22 22:30:09 +0100

    Made into a function which accepts a cell array of filenames, now plots both magnitude of magnetic field and magnitude of perturbed field

--- benchmark/sphere_benchmark/sphere_plot_results.m
+++ benchmark/sphere_benchmark/sphere_plot_results.m
@@ -1,7 +1,9 @@
-filenames = {'results/sphere_bench1345_p0/bench1_diag_xaxis_ptfield_p0.out' ...
-    'results/sphere_bench1345_p0/bench3_diag_xaxis_ptfield_p0.out' ...
-    'results/sphere_bench1345_p0/bench4_diag_xaxis_ptfield_p0.out' ...
-    'results/sphere_bench1345_p0/bench5_diag_xaxis_ptfield_p0.out' };
+function sphere_plot_results(filenames)
+
+% filenames = {'results/sphere_bench1345_p0/bench1_diag_xaxis_ptfield_p0.out' ...
+%     'results/sphere_bench1345_p0/bench3_diag_xaxis_ptfield_p0.out' ...
+%     'results/sphere_bench1345_p0/bench4_diag_xaxis_ptfield_p0.out' ...
+%     'results/sphere_bench1345_p0/bench5_diag_xaxis_ptfield_p0.out' };
 
 % filenames = {'results/sphere_meshrefinement_run1/bench1_diag_ptfield_p0.out' ...
 %     'results/sphere_meshrefinement_run1/bench3_diag_ptfield_p0.out' ...
@@ -13,7 +15,7 @@ filenames = {'results/sphere_bench1345_p0/bench1_diag_xaxis_ptfield_p0.out' ...
 %     'results/bench4_xaxis_ptfield_p0.out'};
 
 
-line_colour = {'.-r' '.-k' '.-g' '.-m'};
+line_colour = {'.-r' '+-k' '*-g' '^-m'};
 
 % load the first:
 data = load(filenames{1});
@@ -22,10 +24,10 @@ He = data(:,20:22)' + 1i*data(:,23:25)';
 He_pt = data(:,32:34)' + 1i*data(:,35:37)';
 
 % Adjust for perturbed:
-H0=[0;0;1];
-for i=1:length(r)
-    He(:,i) = He(:,i)-H0;
-end
+% H0=[0;0;1];
+% for i=1:length(r)
+%     He(:,i) = He(:,i);
+% end
 
 % Magnitudes:
 magHe=zeros(length(r),1);
@@ -34,9 +36,11 @@ for i=1:length(r)
     magHe(i) = norm(He(:,i));
     magHe_pt(i) = norm(He_pt(:,i));
 end
-figure;
-semilogy(r,magHe_pt);
-hold on;
+h1=figure;
+plot(r,magHe,'LineWidth',2);
+h2=figure;
+plot(r,magHe_pt,'LineWidth',2);
+
 
 magHa=zeros(length(r),length(filenames));
 magHa_pt=zeros(length(r),length(filenames));
@@ -47,7 +51,7 @@ for f=1:length(filenames)
     Ha_pt = data(:,26:28)' + 1i*data(:,29:31)';
     % Adjust for perturbed:
     for i=1:length(r)
-        Ha(:,i) = Ha(:,i)-H0;
+        Ha(:,i) = Ha(:,i);
     end
     
     for i=1:length(r)
@@ -55,7 +59,22 @@ for f=1:length(filenames)
         magHa_pt(i,f) = norm(Ha_pt(:,i));
     end
     % Add to plot
-    semilogy(r,magHa(:,f),line_colour{f});
+    figure(h1);
+    hold on;
+    plot(r,magHa(:,f),line_colour{f});
+    hold off;
+    figure(h2);
+    hold on;
+    plot(r,magHa_pt(:,f),line_colour{f});
+    hold off;
+    
 end
 
-legend('PT', 'M1', 'M2', 'M3', 'M4');
+% mesh ref:
+% legend('PT', 'M1', 'M2', 'M3', 'M4');
+
+% p-ref:
+legend('PT', 'p=0', 'p=1', 'p=2');
+
+
+end
